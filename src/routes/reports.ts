8d310fe7f4b4552f6b1e9d3b05eb3af0785351f2
Beatriz <beatrizamante@hotmail.com>
2025-03-11 16:06:13 -0300

    Creating serializers

--- src/routes/reports.ts
+++ src/routes/reports.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from 'fastify';
 import reportFetch from '../actions/products/fetch';
 
 export default async function reportsRoutes(server: FastifyInstance) {
-  server.post('/sales', reportFetch);
-  server.post('/top-products', reportFetch);
+  server.get('/sales', reportFetch);
+  server.get('/top-products', reportFetch);
 
 }
