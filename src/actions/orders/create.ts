cbc51af2063819b0a5f93858ed25f616360823eb
Beatriz <beatrizamante@hotmail.com>
2025-03-11 13:46:18 -0300

    Creating an update/insert for order creation

--- src/actions/orders/create.ts
+++ src/actions/orders/create.ts
@@ -4,13 +4,14 @@ import { CouldNotCreate, ERROR_CODES } from '../../errors/errors';
 
 type Request = FastifyRequest<{
   Body: {
+    id?: number;
     customer_id: number;
     items: { product_id: number; quantity: number; discount?: number }[];
   };
 }>;
 
 export default async (
-  { body: { customer_id, items } }: Request,
+  { body: { id, customer_id, items } }: Request,
   reply: FastifyReply
 ) => {
   const trx = await Order.startTransaction();
@@ -18,23 +19,50 @@ export default async (
   try {
     const productsId = items.map((item) => item.product_id);
     const productPrices = new Map(
-      (await Product.query(trx).select('id', 'price').whereIn('id', productsId))
-        .map(({ id, price }) => [id, price])
+      (
+        await Product.query(trx).select('id', 'price').whereIn('id', productsId)
+      ).map(({ id, price }) => [id, price])
     );
-    const totalDiscount = items.reduce((sum, item) => sum + (item.discount || 0), 0);
-    const totalPaid = items.reduce((sum, { product_id, quantity }) => {
-      const price = productPrices.get(product_id) || 0;
-      return sum + price * quantity;
-    }, 0) - totalDiscount;
+    const totalDiscount = items.reduce(
+      (sum, item) => sum + (item.discount || 0),
+      0
+    );
+    const totalPaid =
+      items.reduce((sum, { product_id, quantity }) => {
+        const price = productPrices.get(product_id) || 0;
+        return sum + price * quantity;
+      }, 0) - totalDiscount;
 
-    const order = await Order.query(trx).insert({
-      customer_id,
-      total_paid: totalPaid as number,
-      total_discount: totalDiscount as number,
-      total_shipping: 0,
-      total_tax: 0,
-      status: OrderStatus.PaymentPending,
-    });
+    let order;
+
+    if (id) {
+      order = await Order.query(trx).findById(id);
+
+      if (!order)
+        throw new Error('Order not found. Please, create a new order.');
+      if (order.status !== OrderStatus.PaymentPending)
+        throw new Error('Only pending payments can be modified.');
+
+      order = await Order.query(trx).patchAndFetchById(id, {
+        customer_id,
+        total_paid: totalPaid as number,
+        total_discount: totalDiscount as number,
+        total_shipping: 0,
+        total_tax: 0,
+        status: order.status,
+      });
+
+      await OrderItem.query(trx).where('order_id', id).delete();
+    } else {
+      order = await Order.query(trx).insert({
+        customer_id,
+        total_paid: totalPaid as number,
+        total_discount: totalDiscount as number,
+        total_shipping: 0,
+        total_tax: 0,
+        status: OrderStatus.PaymentPending,
+      });
+    }
 
     const orderItem = items.map((item) => ({
       order_id: order.id,
@@ -56,12 +84,16 @@ export default async (
       total_paid: totalPaid,
       total_discount: totalDiscount,
       status: order.status,
-      items
+      items,
     });
   } catch (error) {
     await trx.rollback();
 
-    if (error instanceof CouldNotCreate) throw new CouldNotCreate(ERROR_CODES.COULD_NOT_CREATE, 'Error inserting data.');
+    if (error instanceof CouldNotCreate)
+      throw new CouldNotCreate(
+        ERROR_CODES.COULD_NOT_CREATE,
+        'Error inserting data.'
+      );
 
     throw error;
   }
